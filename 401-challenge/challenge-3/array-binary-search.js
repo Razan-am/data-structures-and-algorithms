ed2208bde6bd22cf7b9e5313f4d774ab109af357
razan-am <razanalamleh@gmail.com>
2021-08-14 21:55:58 +0300

    edite challenge 3

--- 401-challenge/challenge-3/array-binary-search.js
+++ 401-challenge/challenge-3/array-binary-search.js
@@ -1,15 +1,23 @@
 'use strict';
 
-let array = [5,7,4];
-let x = 7;
+let array = [5,7,2,8,4];
+let x = 2;
 
 function BinarySearch (array,x) {
-     for(let i =0; i<array.length; i++){
-          if(array[i] === x){
-                  return i;
-          }else{
-                  return -1;
-          }
-    }
+     let first = 0;
+     let last = array.length - 1;
+     while(first <= last){
+          let middle = Math.floor((first+last)/2);
+          if(array[middle] === x){
+                return x;
+          }else if(array[middle] < x){ 
+                  return first = middle + 1;
+           }else{
+                  return last = middle - 1;
+           }
+      }
+     return -1;
 }
-BinarySearch (array,7)
+BinarySearch (array,2)
+
+
