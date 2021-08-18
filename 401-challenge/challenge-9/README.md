8cb4b54c51e10e26886ea2eb627865d82f122ad4
razan-am <razanalamleh@gmail.com>
2021-08-18 20:31:57 +0300

    reverse linkedlist

---
+++ 401-challenge/challenge-9/README.md
@@ -0,0 +1,18 @@
+# Code challenge 9:
+
+## reverse-linked-list
+<!-- Description of the challenge -->
+- To write a function to reverse a Singly Linked List.
+
+### Whiteboard Process
+<!-- Embedded whiteboard image -->
+
+![image](/images/reverse-ll.png)
+
+### Approach & Efficiency
+<!-- What approach did you take? Discuss Why. What is the Big O space/time for this approach? -->
+- I took the output input approach, it will show the result how it should be 
+
+- Big O 
+   - Time <--- O(1)
+   - Space <----- O(1)
---
+++ 401-challenge/challenge-9/__test__/reverse.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const LinkedList= require('../reverse-ll');
+
+describe('linked list module',()=>{
+
+    it('constructor()',()=>{
+        let newList = new LinkedList();
+        expect(newList.head).toEqual(null);
+    })
+
+    it('append',()=>{
+        let newList = new LinkedList();
+        let initialValue = 'first value';
+        newList.append(initialValue);
+        expect(newList.head.value).toEqual(initialValue);
+    })
+
+    it('revers list',()=>{
+        let newList = new LinkedList();
+        newList.insert(1);
+        newList.append(2);
+        newList.append(3);
+        newList.append(4);
+        expect(newList.reversList(newList).toString()).toEqual( "{4}->{3}->{2}->{1}")
+    })
+
+})
